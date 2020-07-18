3d09b64257dceaf65f389f6c0f2b9868bac24bf0
vashudevdhama <dhamavashu1119@gmail.com>
2020-07-18 22:19:32 +0530

    Interactions added

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function showPage() {
+  console.log("showPage");
+  var x = document.getElementById("main");
+  x.style.display = "block";
+  var y = document.getElementById("clickme");
+  y.style.display = "none";
+}
