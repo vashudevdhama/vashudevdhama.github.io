a534402011b463b809a4ef565ab36413799bf80b
vashudevdhama <dhamavashu1119@gmail.com>
2020-07-22 00:10:12 +0530

    v2:01

--- js/main.js
+++
@@ -1,7 +0,0 @@
-function showPage() {
-  console.log("showPage");
-  var x = document.getElementById("main");
-  x.style.display = "block";
-  var y = document.getElementById("clickme");
-  y.style.display = "none";
-}
